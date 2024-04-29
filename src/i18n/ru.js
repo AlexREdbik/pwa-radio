74ce1c9f663da28490e41372389bd95ebf6d3385
AlexREd <redbik@redbik.eu>
2024-04-29 21:40:02 +0300

    Create ru.js

---
+++ src/i18n/ru.js
@@ -0,0 +1,15 @@
+export const ru = {
+    pick_a_station: 'Выберите станцию',
+    view_stations: 'Просмотр станций',
+    search_a_station: 'Поиск станции',
+    recent_tracks: 'Последние треки',
+    nextTrack: 'Далее',
+    choose_animation: 'Выбрать анимацию',
+    disable: 'Отключить',
+    cloud: 'Облако',
+    waves: 'Волны',
+    halo: 'HALO',
+    trunk: 'TRUNK',
+    tw_listening_to : 'Entrain d\'écouter ',
+    live_on : 'Прямой эфир'
+}
